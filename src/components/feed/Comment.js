abd4d8a17c9e6f474a8d6956a0c940a5902bde21
UniverseStark <jty90k@gmail.com>
2021-05-13 16:47:17 +0900

    11.14 Parsing Hashtags part Two

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,17 +1,19 @@
-import sanitizeHtml from "sanitize-html";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
   ${"" /* hasTag글씨 변경해주는 로직 */}
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     ${"" /* 마우스 커서 변경 : 화살표 -> 손모양 */}
     cursor: pointer;
+    ${"" /* 마우스 커서 이동시 hasTag에 밑줄이 생긴다. */}
     &:hover {
       text-decoration: underline;
     }
@@ -19,22 +21,20 @@ const CommentCaption = styled.span`
 `;
 
 function Comment({ author, payload }) {
-  // hasTag기능 예시) #I love shop, 무분별한 html을 없애주고 hasTag만 남게 해준다.
-  const cleanedPayload = sanitizeHtml(
-    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
-    {
-      allowedTags: ["mark"],
-    }
-  );
-
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleanedPayload,
-        }}
-      />
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
