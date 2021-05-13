52ad36af1d1a37362a7fd8f2d679e7932948bf1c
UniverseStark <jty90k@gmail.com>
2021-05-13 15:52:47 +0900

    11.13 Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
@@ -5,13 +6,35 @@ import { FatText } from "../shared";
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  ${"" /* hasTag글씨 변경해주는 로직 */}
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    ${"" /* 마우스 커서 변경 : 화살표 -> 손모양 */}
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }) {
+  // hasTag기능 예시) #I love shop, 무분별한 html을 없애주고 hasTag만 남게 해준다.
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[\w]+/g, "<mark>$&</mark>"),
+    {
+      allowedTags: ["mark"],
+    }
+  );
+
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 }
