d58abe5e7d3727ae5c0239124145ef83d6b9be3e
UniverseStark <jty90k@gmail.com>
2021-05-14 18:00:33 +0900

    11.16 Create Comment part One

--- src/components/feed/Comments.js
+++ src/components/feed/Comments.js
@@ -1,6 +1,17 @@
+import { gql, useMutation } from "@apollo/client";
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Comment from "./Comment";
+
+const CREATE_COMMENT_MUTATION = gql`
+  mutation createComment($photoId: Int!, $payload: String!) {
+    createComment(photoId: $photoId, payload: $payload) {
+      ok
+      error
+    }
+  }
+`;
 
 const CommentsContainer = styled.div`
   margin-top: 20px;
@@ -13,7 +24,26 @@ const CommentCount = styled.span`
   font-size: 10px;
 `;
 
-function Comments({ author, caption, commentNumber, comments }) {
+function Comments({ photoId, author, caption, commentNumber, comments }) {
+  // 아래 두 번째 reteurn 전까지 comments 생성 로직
+  const [createCommentMutation, { loading }] = useMutation(
+    CREATE_COMMENT_MUTATION
+  );
+  const { register, handleSubmit, setValue } = useForm();
+  const onValid = (data) => {
+    const { payload } = data;
+    if (loading) {
+      return;
+    }
+    createCommentMutation({
+      variables: {
+        photoId,
+        payload,
+      },
+    });
+    //엔터를 눌러도 바로 comment
+    setValue("payload", "");
+  };
   return (
     <CommentsContainer>
       <Comment author={author} payload={caption} />
@@ -27,11 +57,23 @@ function Comments({ author, caption, commentNumber, comments }) {
           payload={comment.payload}
         />
       ))}
+      {/* comments : 댓글 쓰는 로직 */}
+      <div>
+        <form onSubmit={handleSubmit(onValid)}>
+          <input
+            name="payload"
+            ref={register({ required: true })}
+            type="text"
+            placeholder="Write a comment..."
+          />
+        </form>
+      </div>
     </CommentsContainer>
   );
 }
 
 Comments.propTypes = {
+  photoId: PropTypes.number.isRequired,
   author: PropTypes.string.isRequired,
   caption: PropTypes.string,
   commentNumber: PropTypes.number.isRequired,
